c8aa21bb563d2c59e3d6871f8269f9f3da8169a2
Zhao Xin <7176466@qq.com>
2021-03-08 17:37:10 +0800

    Initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="fixed top-0 left-0 flex w-full gap-4 p-4 bg-gray-200 shadow-md">
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/helloworld">Hello</NuxtLink>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+    }
+</script>
+
+<style lang="postcss" scoped>
+    a {
+        @apply bg-gray-100 border border-gray-300 rounded shadow-xs px-4 py-2 text-green-400;
+    }
+    .nuxt-link-exact-active {
+        @apply bg-gray-300 border-gray-100 shadow-inner text-green-700 font-bold;
+    }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+export default {
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "Nuxt Playground",
+        htmlAttrs: {
+            lang: "en"
+        },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: "Nuxt Playground"
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    },
+
+    // Dev Server config: https://zh.nuxtjs.org/docs/2.x/configuration-glossary/configuration-server
+    server: {
+        port: 8080, // default: 3000
+        host: "0.0.0.0" // default: localhost
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/tailwindcss
+        "@nuxtjs/tailwindcss"
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        "@nuxtjs/axios"
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {}
+};
