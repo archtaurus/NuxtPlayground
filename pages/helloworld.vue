cfaec5d423ad3781caf780fbe22446470895a802
Zhao Xin <7176466@qq.com>
2021-03-09 21:14:34 +0800

    update

---
+++ pages/helloworld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div>
+            <h1 class="title">Hello World</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        head: {
+            title: 'Hello World',
+        },
+    }
+</script>
